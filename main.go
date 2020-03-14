ab2f28fafaee8571d6a1b4d5ae5080b4566be731
Hưng Lê <hungle.info@gmail.com>
2020-03-15 01:25:55 +0700

    Support language and limit flags

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"encoding/json"
+	"errors"
+	"flag"
 	"fmt"
+	"log"
+	"strconv"
+
 	"github.com/go-resty/resty/v2"
 	. "github.com/logrusorgru/aurora"
-	"log"
 )
 
 // Refer to this.
@@ -28,29 +31,38 @@ type Response struct {
 	Data Children
 }
 
+var (
+	lang  string
+	limit int
+)
+
+func init() {
+	flag.StringVar(&lang, "lang", "golang", "Programming Language")
+	flag.IntVar(&limit, "limit", 10, "Limit")
+	flag.Parse()
+}
+
 func main() {
-	target := "https://www.reddit.com/r/golang/new/.json?limit=10"
-	client := resty.New()
+	target := "https://www.reddit.com/r/{lang}/new/.json"
 
 	// https://github.com/go-resty/resty#simple-get
-	response, err := client.R().Get(target)
+	resp, err := resty.New().R().
+		SetPathParams(map[string]string{"lang": lang}).
+		SetQueryParam("limit", strconv.Itoa(limit)).
+		SetResult(&Response{}).
+		Get(target)
 	if err != nil {
 		log.Fatal(err)
+		return
+	}
+
+	if response, ok := resp.Result().(*Response); !ok {
+		log.Fatal(errors.New("invalid response format"))
+		return
 	} else {
-		// fmt.Printf("%s\n", response) // body
-
-		bodyBytes := response.Body()
-		response := Response{}
-
-		err := json.Unmarshal(bodyBytes, &response)
-		if err != nil {
-			log.Fatal(err)
-		}
-		// fmt.Println(response.Data)
-
 		for index, post := range response.Data.Children {
 			num := index + 1
-			stdoutLink := fmt.Sprintf("%d. %s(%s)", num, post.Data.Title, Blue(post.Data.Url))
+			stdoutLink := fmt.Sprintf("%d. %s (%s)", num, post.Data.Title, Blue(post.Data.Url))
 			fmt.Println(stdoutLink)
 		}
 	}
